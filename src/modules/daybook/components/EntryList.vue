ce702d52b397aa789de83d064413133e8d5a6e02
masvidal778 <nuria.masvidal@gmail.com>
2023-09-15 17:10:50 +0200

    Creating EntryList and Individual Entries

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,15 +1,41 @@
 <script>
-import {defineComponent} from 'vue'
+import {defineComponent, defineAsyncComponent} from 'vue'
 
 export default defineComponent({
-  name: "EntryList"
+  name: "EntryList",
+  components: {
+    IndividualEntry: defineAsyncComponent( () => import('./IndividualEntry.vue'))
+  }
 })
 </script>
 
 <template>
-<h1>EntryList</h1>
+<div class="entry-list-container">
+  <div class="px-2 pt-2">
+    <input type="text"
+           class="form-control"
+           placeholder="Buscar entrada"
+    >
+  </div>
+
+  <div class="entry-scrollarea">
+    <IndividualEntry
+        v-for="item in 100"
+        :key="item"
+    />
+
+  </div>
+</div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.entry-list-container{
+  border-right: 1px solid #2c3e50;
+  height: calc(100vh - 56px);
+}
 
+.entry-scrollarea{
+  height: calc(100vh - 111px);
+  overflow: scroll;
+}
 </style>
---
+++ src/modules/daybook/components/IndividualEntry.vue
@@ -0,0 +1,47 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "IndividualEntry"
+})
+</script>
+
+<template>
+
+  <div
+      class="entry-container mb-3 pointer p-2"
+  >
+    <!-- titol -->
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">15</span>
+      <span class="mx-1 fs-5">Setembre</span>
+      <span class="mx-2 fw-light">2023, divendres</span>
+    </div>
+
+    <div class="entry-description">
+      Morbi ipsum ex, facilisis a ante dapibus, vehicula aliquet nunc. Vivamus imperdiet lacus augue, eget venenatis tortor elementum non. Morbi in turpis libero. Suspendisse purus sem, euismod a nisl vel, pretium sodales mauris. Aliquam sagittis ligula ac leo consectetur, at placerat leo egestas. Maecenas consequat dignissim neque et luctus. Quisque sit amet varius mi. Ut vitae lacinia purus.
+    </div>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+.entry-container{
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s all ease-in;
+
+  &:hover{
+    background-color: lighten($color: grey, $amount: 45);
+    transition: 0.2s all ease-in;
+  }
+
+  .entry-description{
+    font-size: 12px;
+  }
+
+}
+
+
+
+
+</style>
