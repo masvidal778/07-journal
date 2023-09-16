73381c24d4646de1cead0abd2f236bdbce9cd44f
masvidal778 <nuria.masvidal@gmail.com>
2023-09-16 20:59:08 +0200

    getting entries using mapGetters and filtering by terms

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -3,7 +3,11 @@
 //return state
 //}
 
-export const getEntriesByTerm = ( /*state*/ ) => {
+export const getEntriesByTerm = ( state ) => ( term = '' ) => {
+    if( term.length === 0 ) return state.entries
+
+    return state.entries.filter( entry =>
+        entry.text.toLowerCase().includes( term.toLocaleLowerCase() ) )
 
 }
 
