a7ad69355ec658573e4ad0ade5105416aa623d2e
masvidal778 <nuria.masvidal@gmail.com>
2023-10-18 10:58:07 +0200

    testing if routes contains the correct entries

--- tests/unit/modules/daybook/routers/index.spec.js
+++ tests/unit/modules/daybook/routers/index.spec.js
@@ -6,24 +6,33 @@ describe('testing router module/daybook', () => {
 
         expect(daybookRouter).toMatchObject({
             name: 'daybook',
-            component: expect.any( Function ),
+            component: expect.any(Function),
             children: [
                 {
                     path: '',
                     name: 'no-entry',
-                    component: expect.any( Function )
+                    component: expect.any(Function)
                 },
                 {
                     path: ':id',
                     name: 'entry',
-                    component: expect.any( Function ),
-                    props: expect.any( Function )
+                    component: expect.any(Function),
+                    props: expect.any(Function)
                 },
             ]
         })
 
-        expect((await daybookRouter.children[0].component()).default.name ).toBe('NoEntrySelected')
-        expect((await daybookRouter.children[1].component()).default.name ).toBe('EntryView')
+
+        //expect((await daybookRouter.children[0].component()).default.name ).toBe('NoEntrySelected')
+        //expect((await daybookRouter.children[1].component()).default.name ).toBe('EntryView')
+        const promiseRoutes = []
+        daybookRouter.children.forEach(child => promiseRoutes.push(child.component()))
+
+        //Promise.all() executa totes les promeses definides a l'array
+        const routes = (await Promise.all(promiseRoutes)).map(r => r.default.name)
+
+        expect(routes).toContain('EntryView')
+        expect(routes).toContain('NoEntrySelected')
 
     })
 
