ec6a86f557b4858d8fdcc35080a1c2e972e6fd16
masvidal778 <nuria.masvidal@gmail.com>
2023-10-24 17:38:15 +0200

    fixing cloudinary issue

--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -4,15 +4,15 @@ import axios from 'axios'
 import uploadImage from "@/modules/daybook/helpers/uploadImage";
 
 cloudinary.config({
-    cloud_name: 'dqkxcuput',
-    api_key: '681921631383886',
-    api_secret: 'YjWMalUPUETzOhBEtsu5sL65r2c'
+    cloud_name: 'xxx',
+    api_key: 'xxx',
+    api_secret: 'xxx'
 })
 
 describe("testing uploadImage", () => {
 
     test('should upload a file and return url', async() => {
-        const { data } = await axios.get('https://res.cloudinary.com/dqkxcuput/image/upload/v1697048344/wibhrg2otqmhoqdcg3kc.jpg',{
+        const { data } = await axios.get('https://res.cloudinary.com/xxx/image/upload/v1697048344/wibhrg2otqmhoqdcg3kc.jpg',{
             responseType: 'arraybuffer'
         })
 
