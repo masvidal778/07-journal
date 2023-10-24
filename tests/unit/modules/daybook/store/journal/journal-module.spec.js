4ecf6ea7d5de08057741b63617057d05de3d1c9f
masvidal778 <nuria.masvidal@gmail.com>
2023-10-24 11:37:13 +0200

    details in journal's store state

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -1,14 +1,14 @@
 import { createStore } from "vuex";
 import journal from "@/modules/daybook/store/journal";
-import { journalState } from "../../../../mock-data/test-journal-state";
+import { journalState } from "../../../../mock-data/test-journal-state";;
 
-const createVuexStore = ( initialState ) =>
+const createVuexStore = (initialState) =>
     createStore({
         modules: {
             journal: {
                 //desestructurem journal per sobreescriure l'state amb el que es tingui a initialState
                 ...journal,
-                state: { ...initialState }
+                state: {...initialState}
             }
         }
     })
@@ -18,27 +18,11 @@ describe('Vuex, testing journal module', () => {
     test('this is the initial state, it should have to this state', () => {
 
         //Es crea un vuex store per treballar amb les proves
-        const store = createVuexStore( journalState )
-        const { isLoading, entries } = ( store.state.journal )
+        const store = createVuexStore(journalState)
+        const {isLoading, entries} = (store.state.journal)
 
-        expect( isLoading ).toBeFalsy
-        expect( entries ).toEqual( journalState.entries )
-    })
-
-    //Mutations
-    test('Mutations: setEntries', () => {
-
-        const store = createVuexStore({ isLoading: true, entries: [] })
-
-        store.commit('journal/setEntries', journalState.entries )
-
-        expect(store.state.journal.entries.length).toBe(2)
-        expect(store.state.journal.isLoading).toBeFalsy()
-
-    })
-
-    test('', () => {
-
+        expect(isLoading).toBeFalsy
+        expect(entries).toEqual(journalState.entries)
     })
 
 })
