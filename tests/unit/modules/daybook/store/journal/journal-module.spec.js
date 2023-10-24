2b3cb5db05621cbd3ceae8dcb35b6e09c065783e
masvidal778 <nuria.masvidal@gmail.com>
2023-10-24 11:52:10 +0200

    testing store mutation updatedEntry

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -36,6 +36,30 @@ describe('Vuex, testing journal module', () => {
         expect(store.state.journal.isLoading).toBeFalsy()
 
     })
-    
+
+    test('mutation: updateEntry', () => {
+
+        //create store con entries
+        const store = createVuexStore( journalState )
+
+        //updatedEntry
+        const updatedEntry = {
+            id: 'ABC123',
+            date: "Sun Jul 18 2021",
+            text: 'Hola, món! Des de les proves',
+            picture: "https://res.cloudinary.com/dqkxcuput/image/upload/v1697048344/wibhrg2otqmhoqdcg3kc.jpg"
+        }
+
+        //commit de la mutació
+        store.commit('journal/updateEntry', updatedEntry)
+
+        //entries.length = 2
+        expect(store.state.journal.entries.length).toBe(2)
+        //entries tiene que existir el updatedEntry toEqual
+        expect(
+            store.state.journal.entries.find( e => e.id === updatedEntry.id)
+            ).toEqual(updatedEntry)
+
+    })
 
 })
